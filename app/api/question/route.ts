e029eefdbca2e8f85575a8bb418fd58d50efd007
githubmor <m.darzi@pooyasamaneh.net>
2023-04-09 14:01:31 +0330

    a

--- app/api/question/route.ts
+++ app/api/question/route.ts
@@ -4,9 +4,9 @@ if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing env var from OpenAI");
 }
 
-export const config = {
-  runtime: "edge",
-};
+// export const config = {
+//   runtime: "edge",
+// };
 
 export async function POST(req: Request): Promise<Response> {
   const { prompt } = (await req.json()) as {
